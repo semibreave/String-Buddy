2c2d06c4e701e1aff259bed99c9e00058e2903c7
semibreave <91180380+semibreave@users.noreply.github.com>
2022-12-16 10:56:06 +0800

    Add files via upload

---
+++ string delimit.ps1
@@ -0,0 +1,36 @@
+﻿$arr =@()
+$vm =""
+
+$str= Read-Host "Enter string"
+$delim = Read-Host "Enter delimiter"
+$slen = $str.Length
+
+while($slen -gt 0){
+
+    if($str.LastIndexOf($delim) -ge 0){
+
+        $vm = $str.Substring($str.LastIndexOf($delim))
+
+        $arr += $vm
+
+        $slen -= $vm.Length
+
+        $str = $str.Substring(0,$str.LastIndexOf($delim))
+    }
+    
+    else{
+        
+        $vm = $str
+
+        $arr += $vm
+
+        $slen -= $vm.Length
+
+    }
+    
+
+}
+
+    [array]::Reverse($arr)
+
+    $arr
